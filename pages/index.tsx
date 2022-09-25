97fbf89ff0c27b959810c14d15bc55da555a81c6
kevvvinreed <33916889+kevvvinreed@users.noreply.github.com>
2022-09-24 17:33:43 -0700

    matrix animation masked by 'ALGOCIDE' custom cursor work started

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,28 +2,96 @@ import { createEntries } from '@/src/db/create/createEntries';
 import { deleteEntries } from '@/src/db/delete/deleteEntries';
 import { readEntries } from '@/src/db/read/readEntries';
 import { updateEntries } from '@/src/db/update/updateEntries';
+import useCursor from '@/src/hooks/useCursor';
+import useWindow from '@/src/hooks/useWindow';
 import { NextPage } from 'next';
 import Head from 'next/head';
 import { IEntry } from 'prisma/schema';
-import { useState } from 'react';
+import {
+  LegacyRef,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
 import styles from '../src/styles/pages/index.module.css';
 import { IPageProps } from './_app';
 
-const Home: NextPage<IPageProps> = ({}) => {
-  const [inputBuffer, setInputBuffer] = useState<IEntry[]>();
-  const [input, setInput] = useState<string>('');
+const animate = (ref: React.MutableRefObject<any>) => {
+  const ctx = ref.current.getContext('2d');
+  const height = ref.current.height;
+  const width = ref.current.width;
 
-  const [dataUpdateInput, setDataUpdateInput] = useState<string>('');
-  const [whereUpdateInput, setWhereUpdateInput] = useState<string>('');
-  const [deleteInput, setDeleteInput] = useState<string>('');
-
-  const add_to_input = (input_var: string) => {
-    setInputBuffer(prevInput =>
-      prevInput
-        ? [...prevInput, { id: input_var, name: '' }]
-        : [{ id: input_var, name: '' }]
+  const characters =
+    `abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%+-/~{[|\`]}`.split(
+      ''
     );
+
+  const font_size = 10;
+  const columns = Math.floor(width / font_size);
+
+  const drops: any[] = [];
+
+  for (let x = 0; x < columns; x++) {
+    drops[x] = 1;
+  }
+
+  const draw = () => {
+    //Black BG for the canvas
+    //translucent BG to show trail
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.04)';
+    ctx.fillRect(0, 0, width, height);
+
+    ctx.fillStyle = '#64d86b'; //green text
+    ctx.font = font_size + 'px arial';
+    //looping over drops
+    for (let i = 0; i < drops.length; i++) {
+      //a random chinese character to print
+      let text = characters[Math.floor(Math.random() * characters.length)];
+      //x = i*font_size, y = value of drops[i]*font_size
+      ctx.fillText(text, i * font_size, drops[i] * font_size);
+
+      //sending the drop back to the top randomly after it has crossed the screen
+      //adding a randomness to the reset to make the drops scattered on the Y axis
+      if (drops[i] * font_size > height && Math.random() > 0.975) drops[i] = 0;
+
+      //incrementing Y coordinate
+      drops[i]++;
+    }
   };
+  setInterval(draw, 35);
+};
+
+const Home: NextPage<IPageProps> = ({}) => {
+  const canvasRef = useRef<any>();
+  const textRef1 = useRef<any>();
+
+  const [text1, setText1] = useState<{
+    x?: number;
+    y?: number;
+    width?: number;
+    height?: number;
+  }>({});
+
+  const { windowWidth, windowHeight } = useWindow();
+  const { cursorX, cursorY } = useCursor();
+
+  useEffect(() => {
+    if (canvasRef) {
+      animate(canvasRef);
+    }
+  }, [canvasRef, windowWidth, windowHeight]);
+
+  useEffect(() => {
+    if (textRef1 && textRef1.current && windowWidth) {
+      const width = textRef1.current.getComputedTextLength();
+      const x = windowWidth / 2 - width / 2;
+      setText1({ x, width });
+    }
+  }, [textRef1, windowWidth]);
+
+  useEffect(() => {}, [cursorX, cursorY]);
 
   return (
     <>
@@ -32,85 +100,89 @@ const Home: NextPage<IPageProps> = ({}) => {
         <meta name="description" content="Home Page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={styles.container}>
-        <div style={{ color: `var(--accent)` }}>
-          {JSON.stringify(inputBuffer)}
-        </div>
-        <input
-          onChange={e => setInput(e.target.value)}
-          value={input}
-          placeholder="Input ID"
-        ></input>
-        <button
-          onClick={() => {
-            if (input === '') {
-              return;
-            }
-            add_to_input(input);
-            setInput('');
+      <div
+        className={styles.container}
+        // style={{
+        //   background: `radial-gradient(circle at ${cursorX}px ${cursorY}px, rgba(255,255,255,1) 20px, rgba(0,0,0,1) 150px)`,
+        // }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={windowWidth}
+          height={windowHeight}
+          viewBox={`0 0 ${windowWidth} ${windowHeight}`}
+          style={{
+            position: 'relative',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
           }}
         >
-          Submit
-        </button>
-        <br />
-        <button
-          onClick={async () => {
-            if (inputBuffer) {
-              createEntries(inputBuffer);
-            }
-          }}
-        >
-          Create
-        </button>
-        <br />
-        <button
-          onClick={async () => {
-            const res = await readEntries({ amount: 10 });
-          }}
-        >
-          Read
-        </button>
-        <br />
+          <text
+            id="algocide"
+            ref={textRef1}
+            x={text1.x}
+            y="200"
+            fontSize={`${windowWidth / 10}px`}
+            letterSpacing={`${windowWidth / 125}px`}
+            fontFamily="Skygraze"
+            paintOrder="stroke"
+            stroke="white"
+            strokeWidth="2px"
+            strokeLinecap="butt"
+            strokeLinejoin="miter"
+          >
+            {`ALGOCIDE`}
+          </text>
+          <circle
+            cx={cursorX}
+            cy={cursorY}
+            r="30px"
+            stroke="white"
+            strokeWidth="2px"
+            strokeLinecap="butt"
+            strokeLinejoin="miter"
+            style={{ transition: ' 0.02s linear' }}
+            // points={`${cursorX - 30},${cursorY - 30}, ${cursorX + 30},${
+            //   cursorY - 30
+            // }, ${cursorX + 30},${cursorY + 30}, ${cursorX - 30},${
+            //   cursorY + 30
+            // }`}
+          />
+          <clipPath id="clip-path">
+            <circle
+              cx={cursorX}
+              cy={cursorY}
+              r="30px"
+              style={{ transition: ' 0.02s linear' }}
+              // points={`${cursorX - 30},${cursorY - 30}, ${cursorX + 30},${
+              //   cursorY - 30
+              // }, ${cursorX + 30},${cursorY + 30}, ${cursorX - 30},${
+              //   cursorY + 30
+              // }`}
+            />
+            <text
+              y="200"
+              x={text1.x}
+              fontSize={`${windowWidth / 10}px`}
+              letterSpacing={`${windowWidth / 125}px`}
+              fontFamily="Skygraze"
+            >
+              {`ALGOCIDE`}
+            </text>
+          </clipPath>
+        </svg>
 
-        <div style={{ color: `var(--accent)` }}>
-          {`From ID: ${whereUpdateInput}`}
-          <br />
-          {`To name: ${dataUpdateInput}`}
-        </div>
-        <input
-          onChange={e => setWhereUpdateInput(e.target.value)}
-          value={whereUpdateInput}
-          placeholder="From ID"
-        ></input>
-        <input
-          onChange={e => setDataUpdateInput(e.target.value)}
-          value={dataUpdateInput}
-          placeholder="To Name"
-        ></input>
-        <button
-          onClick={() => {
-            updateEntries({
-              where: { id: whereUpdateInput },
-              data: { name: dataUpdateInput },
-            });
-          }}
-        >
-          Update
-        </button>
-        <br />
-        <div style={{ color: `var(--accent)` }}>{`Delete: ${deleteInput}`}</div>
-        <input
-          onChange={e => setDeleteInput(e.target.value)}
-          value={deleteInput}
-          placeholder="Delete ID"
-        ></input>
-        <button
-          onClick={() => {
-            deleteEntries({ where: { id: deleteInput } });
-          }}
-        >
-          Delete
-        </button>
+        <div
+          className={styles.cursorDot}
+          style={{ left: cursorX - 1.5, top: cursorY - 1.5 }}
+        />
+        <canvas
+          width={windowWidth}
+          height={windowHeight}
+          ref={canvasRef}
+          className={styles.matrix}
+        />
       </div>
     </>
   );
